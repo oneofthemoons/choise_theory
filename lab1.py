5226867dbc3c6dd4547492755e278931149a17ed
Maxim Grechanichenko <ootmoons@gmail.com>
2021-09-05 04:44:09 +0300

    add ge le, add equaling table

--- lab1.py
+++ lab1.py
@@ -38,10 +38,20 @@ class Alternative:
         return rv
 
     def __le__(self, other: Alternative) -> bool:
-        return self < other or self == other
+        rv = True
+        for i in range(len(self.criteria)):
+            gt_crit = self.criteria[i] <= other.criteria[i] and self.config[i] \
+                or self.criteria[i] >= other.criteria[i] and not self.config[i]
+            rv = rv and gt_crit
+        return rv
         
     def __ge__(self, other: Alternative) -> bool:
-        return self > other or self == other
+        rv = True
+        for i in range(len(self.criteria)):
+            gt_crit = self.criteria[i] >= other.criteria[i] and self.config[i] \
+                or self.criteria[i] <= other.criteria[i] and not self.config[i]
+            rv = rv and gt_crit
+        return rv
 
     def __ne__(self, other: Alternative) -> bool:
         return not self == other
@@ -68,7 +78,7 @@ def column_type(col: list) -> Type:
     return str
 
 def make_alternatives(tabled_data: List[List[str]]) -> List[Alternative]:
-    config = tabled_data[0]
+    config = list(map(lambda x: x == '+', tabled_data[0][1:]))
     data = tabled_data[1:]
     types : List[Type] = list()
     for i in range(len(data[0])):
@@ -81,5 +91,46 @@ def make_alternatives(tabled_data: List[List[str]]) -> List[Alternative]:
         alternatives.append(Alternative(row, config))
     return alternatives
 
+def make_equaling_table(alts: List[Alternative]) -> List[List[str]]:
+    table: List[List[str]] = [['' for _ in range(len(alts))] for __ in range(len(alts))]
+    for i in range(len(alts)):
+        for j in range(len(alts)):
+            if j >= i:
+                table[i][j] = 'X'
+            elif alts[i] >= alts[j]:
+                table[i][j] = f'A{i + 1}'
+            elif alts[i] < alts[j]:
+                table[i][j] = f'A{j + 1}'
+            else:
+                table[i][j] = 'H'
+    return table
+
+def print_alternative_ids(alternatives: List[Alternative]) -> None:
+    id = 1
+    n_zeros = len(str(len(alternatives)))
+    for alternative in alternatives:
+        print(f"{str(id).zfill(n_zeros)}: {alternative.variant}")
+        id += 1
+
+def print_equaling_table(eqt: List[List[str]]) -> None:
+    print("Subpair equaling table:")
+    sz_col = max(len(str(len(eqt))), max(map(lambda row: max(map(len, row)), eqt)))
+    col_formatter = "{:_^" + str(sz_col) + "}"
+    print('|' + col_formatter.format(''), end='')
+    for id in range(1, len(eqt) + 1):
+        print('|' + col_formatter.format(str(id)), end='')
+    print()
+    id = 1
+    for row in eqt:
+        print('|' + col_formatter.format(str(id)), end='')
+        for e in row:
+            print('|' + col_formatter.format(e), end='')
+        print()
+        id += 1
+
 if __name__ == '__main__':
-    make_alternatives(parse_data('lab1_data.csv'))
+    alternatives = make_alternatives(parse_data('lab1_data.csv'))
+    
+    print_alternative_ids(alternatives)
+    print()
+    print_equaling_table(make_equaling_table(alternatives))
