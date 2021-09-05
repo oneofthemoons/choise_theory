21f5c50cfa298393ecefe921bd75316f74a0202c
Maxim Grechanichenko <ootmoons@gmail.com>
2021-09-05 06:49:57 +0300

    add alternative filtering

--- lab1.py
+++ lab1.py
@@ -5,15 +5,17 @@ from types import FunctionType
 from typing import *
 
 class Alternative:
-    def __init__(self, variant: str, criteria: List[str], config: List[bool]):
+    def __init__(self, id: int, variant: str, criteria: List[str], config: List[bool]):
         self.variant = variant
         self.criteria = criteria
         self.config = config
+        self.id = id
     
-    def __init__(self, csv_row: List[str], config: List[bool]):
+    def __init__(self, id: int, csv_row: List[str], config: List[bool]):
         self.variant = csv_row[0]
         self.criteria = csv_row[1:]
         self.config = config
+        self.id = id
 
     def __lt__(self, other: Alternative) -> bool:
         rv = True
@@ -85,10 +87,12 @@ def make_alternatives(tabled_data: List[List[str]]) -> List[Alternative]:
         col = [row[i] for row in data]
         types.append(column_type(col))
     alternatives: List[Alternative] = list()
+    id = 1
     for row in data:
         for i in range(1, len(row)):
-            row[i] = types[i](row[i])
-        alternatives.append(Alternative(row, config))
+            row[i] = types[i](row[i].strip())
+        alternatives.append(Alternative(id, row, config))
+        id += 1
     return alternatives
 
 def make_equaling_table(alts: List[Alternative]) -> List[List[str]]:
@@ -113,13 +117,50 @@ def make_Paretho_idx_set(eqt: List[List[str]]) -> Set[int]:
                 rv.add(int(e[1:]) - 1)
     return rv
 
+def make_criteria_filters(criteria: List[str]) -> List[Tuple[int, str, Any]]:
+    print("Идентификаторы критериев:")
+    n_zeros = len(str(len(criteria)))
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    id = 0
+    for c in criteria:
+        print(f"{alphabet[id]}: {c}")
+        id += 1
+    print()
+    print("Введите границы критериев в следующем формате (без скобок):")
+    print("[идентификатор критерия][знак сравнения < или >][значение]")
+    print("Например, несколько критериев можно ввести через пробел: c>12000 d<9")
+    ans = input().split()
+    filters = list()
+    for filter in ans:
+        op_pos = max(filter.find('>'), filter.find('<'))
+        id = alphabet.index(filter[:op_pos])
+        op = filter[op_pos]
+        val = filter[op_pos + 1:]
+        try:
+            val = int(val)
+        except:
+            val = float(val)
+        filters.append((id, op, val))
+    return filters
+
+def make_filtered(alternatives: List[Alternative], filters :List[Tuple[int, str, Any]]):
+    new_alternatives = list()
+    for alternative in alternatives:
+        filtered = False
+        for filter in filters:
+            if filter[1] == '>' and alternative.criteria[filter[0]] <= filter[2]:
+                filtered = True
+            if filter[1] == '<' and alternative.criteria[filter[0]] >= filter[2]:
+                filtered = True
+        if not filtered:
+            new_alternatives.append(alternative)
+    return new_alternatives
+
 def print_alternative_ids(alternatives: List[Alternative]) -> None:
     print("Идентификаторы альтернатив:")
-    id = 1
     n_zeros = len(str(len(alternatives)))
     for alternative in alternatives:
-        print(f"{str(id).zfill(n_zeros)}: {alternative.variant}")
-        id += 1
+        print(f"{str(alternative.id).zfill(n_zeros)}: {alternative.variant}")
 
 def print_equaling_table(eqt: List[List[str]]) -> None:
     print("Таблица попарных сравнений:")
@@ -140,19 +181,31 @@ def print_equaling_table(eqt: List[List[str]]) -> None:
         print('|')
         id += 1
 
-def print_Paretho_set(alternatives: List[Alternative], idx_set: Set[int]) -> None:
-    print("Парето-оптимальное множество решений:")
+def print_set(alternatives: List[Alternative]):
     print("{ ", end='')
-    print(', '.join([alternatives[alt_idx].variant for alt_idx in make_Paretho_idx_set(eqt)]), end='')
+    print(', '.join(alternative.variant for alternative in alternatives), end='')
     print(' }')
 
+def print_Paretho_set(alternatives: List[Alternative], idx_set: Set[int]) -> None:
+    print("Парето-оптимальное множество решений:")
+    print_set([alternatives[alt_idx] for alt_idx in make_Paretho_idx_set(eqt)])
+
 if __name__ == '__main__':
-    alternatives = make_alternatives(parse_data('lab1_data.csv'))
-    
+    data_table = parse_data('lab1_data.csv')
+    alternatives = make_alternatives(data_table)
     print_alternative_ids(alternatives)
     print()
     eqt = make_equaling_table(alternatives)
     print_equaling_table(eqt)
     print()
     print_Paretho_set(alternatives, make_Paretho_idx_set(eqt))
-
+    print()
+    ans = input('Хотите ли вы указать верхние/нижние границы критериев [Д/н]')
+    if any(c in ans for c in 'YyДд'):
+        filters = make_criteria_filters(data_table[0][1:])
+        alternatives = make_filtered(alternatives, filters)
+        print("Альтернативы отфильтрованы.")
+        print_alternative_ids(alternatives)
+        print()
+        print("Текущее множество решений:")
+        print_set(alternatives)
